8f5887facacf8a63efc92fa27ae19359b5d7732c
Siebe88 <siebe.kylstra@gmail.com>
2022-06-05 17:36:49 +0200

    DEV: Another test for auto deploy

--- unwindtime/src/App.js
+++ unwindtime/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> let's test the github deployement, please work
+          Edit <code>src/App.js</code> let's test the github deployement, please please work
         </p>
         <a
           className="App-link"
