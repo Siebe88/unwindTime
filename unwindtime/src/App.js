dd535d0dd22ef71d1228712a6e6c9fe7acda1691
Siebe88 <siebe.kylstra@gmail.com>
2022-06-06 19:02:00 +0200

    Feature: Added Log in screen

--- unwindtime/src/App.js
+++ unwindtime/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Views/Login';
+import Dashboard from './Views/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> let's test the github deployement, super please work
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          {/* <Route exact path="/register" element={<Register />} />
+          <Route exact path="/reset" element={<Reset />} /> */}
+          <Route exact path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
