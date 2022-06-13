838bccb1bb8d908bd96521919f0acb04aecedc19
Siebe88 <siebe.kylstra@gmail.com>
2022-06-13 09:10:23 +0200

    BUILD: ignore for new build

--- unwindtime/src/Components/UnwindsMap.jsx
+++ unwindtime/src/Components/UnwindsMap.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from '@react-google-maps/api';
 
-import Unwind from './Unwind';
+// import Unwind from './Unwind';
 
 const containerStyle = {
   width: '100%',
@@ -16,13 +16,13 @@ function UnwindsMap({ location, unwinds }) {
     googleMapsApiKey: 'AIzaSyCez882QWlP85wQRNooAi0llw1ymzL96zI',
   });
 
-  const [map, setMap] = React.useState(null);
+  const [map, setMap] = React.useState(null); //eslint-disable-line
 
   const onLoad = React.useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds(location);
     map.fitBounds(bounds);
     setMap(map);
-  }, []);
+  }, []); //eslint-disable-line
 
   const onUnmount = React.useCallback(function callback(map) {
     setMap(null);
@@ -31,6 +31,7 @@ function UnwindsMap({ location, unwinds }) {
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={containerStyle}
+      id="41684020cb892eae"
       center={location}
       zoom={14}
       onLoad={onLoad}
