19d622dbbda549048dc561a474562e81ee070b5d
Siebe88 <siebe.kylstra@gmail.com>
2022-07-11 21:30:07 +0200

    BUG: fix reducers for favo relaxMethods

--- unwindtime/src/Services/location.ts
+++ unwindtime/src/Services/location.ts
@@ -1,14 +1,11 @@
-export async function getLocation() {
+export async function getLocation(): Promise<any | undefined> {
+  console.log('getLocation');
   if (!navigator.geolocation) {
     console.log('Geolocation is not supported by your browser');
+    return;
   } else {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        return { lat: position.coords.latitude, lng: position.coords.longitude };
-      },
-      () => {
-        console.log('Unable to retrieve your location');
-      }
-    );
+    navigator.geolocation.getCurrentPosition((position) => {
+      return { lat: position.coords.latitude, lng: position.coords.longitude };
+    });
   }
 }
---
+++ unwindtime/src/Services/services.ts
@@ -0,0 +1,5 @@
+import * as location from './location';
+import * as unwinds from './unwinds';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { location, unwinds };
