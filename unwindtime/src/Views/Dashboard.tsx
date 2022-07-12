7d00b2673b01e5aaaccf0ec76ad8326dbdd12d51
Siebe88 <siebe.kylstra@gmail.com>
2022-07-12 11:39:39 +0200

    Styling: First CSS file removed

--- unwindtime/src/Views/Dashboard.tsx
+++ unwindtime/src/Views/Dashboard.tsx
@@ -1,4 +1,3 @@
-import './Dashboard.css';
 import React, { useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
@@ -12,6 +11,7 @@ import relaxMethods from '../Media/relaxMethodsSVG';
 import { updateProfile } from '../Services/firestore';
 import SetProfilePic from '../Components/SetProfilePic';
 import { GeneralState, RelaxMethods } from '../interfaces/interfaces';
+import { Obj } from 'reselect/es/types';
 
 function Dashboard() {
   const [user, loadingAuth] = useAuthState(auth);
@@ -43,11 +43,30 @@ function Dashboard() {
     dispatch(toggleFavo(relaxMethod));
   };
 
+  const Button = ({
+    extraClasses,
+    buttonText,
+    onClick,
+  }: {
+    extraClasses: String;
+    buttonText: String;
+    onClick: any;
+  }) => {
+    return (
+      <button
+        className={`h-11 w-2/3 rounded-xl text-style-h-3 bg-gray-c-900 text-white drop-shadow-sm shadow-3xl  ${extraClasses}`}
+        onClick={onClick}
+      >
+        {buttonText}
+      </button>
+    );
+  };
+
   return (
     <div className="flex flex-col justify-around items-center h-full">
       <SetProfilePic></SetProfilePic>
 
-      <div className="rounded-xl mx-3 drop-shadow-2xl bg-gray-c-100">
+      <div className="rounded-xl mx-3 drop-shadow-2xl bg-gray-c-100 shadow-3xl">
         <h3 className="m-5 text-style-h-3"> What are your favorite unwind activities?</h3>
         <div className="flex flex-row items-center justify-center flex-wrap px-4 py-4">
           {relaxMethods
@@ -61,24 +80,17 @@ function Dashboard() {
                   classColor={
                     favoRelaxMethods.some((method) => method.name === relaxMethod.name)
                       ? 'favoriteMethod'
-                      : 'nonfavoriteMethod'
+                      : 'nonFavoriteMethod'
                   }
                 />
               );
             })}
         </div>
       </div>
-
-      <button
-        className=" dashboard__btn go_button text-style-h-3 bg-gray-c-900 text-white drop-shadow-sm"
-        onClick={clickEventSaveProfile}
-      >
-        {' '}
-        Okay, let's unwind!
-      </button>
-      <button className="dashboard__btn text-style-h-3 bg-danger-600 text-white drop-shadow-3xl" onClick={logout}>
-        Logout
-      </button>
+      <div className="flex flex-col items-center justify-around w-11/12 h-1/5 ">
+        <Button extraClasses="bg-gray-c-900" buttonText="Okay, let's unwind!" onClick={clickEventSaveProfile}></Button>
+        <Button extraClasses="bg-danger-600" buttonText="Logout" onClick={logout}></Button>
+      </div>
     </div>
   );
 }
--- unwindtime/tailwind.config.js
+++ unwindtime/tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
       white: '#fff',
     },
     extend: {
-      dropShadow: {
+      boxShadow: {
         '3xl': '0px 4px 4px rgba(0, 0, 0, 0.25)',
         '4xl': ['0 35px 35px rgba(0, 0, 0, 0.25)', '0 45px 65px rgba(0, 0, 0, 0.15)'],
       },
