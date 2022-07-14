780106f265b4c0408cb682fbe40b15d004b72259
Siebe88 <siebe.kylstra@gmail.com>
2022-07-14 20:48:20 +0200

    Another CSS file bites the dust 👨‍🚀

--- unwindtime/src/Components/Header.tsx
+++ unwindtime/src/Components/Header.tsx
@@ -3,7 +3,7 @@ import Logo from '../Media/Logo.svg';
 
 export default function Header() {
   return (
-    <div className=" flex justify-center items-center w-full bg-gray-c-900 text-gray-c-100 h-12">
+    <div className=" flex justify-center items-center w-full bg-gray-c-900 text-gray-c-100 h-13">
       <h1 className=" mx-3 mt-1 text-style-h-1">UNWIND</h1>
       <img src={Logo} alt="" />
       <h1 className="mx-3 mt-1 text-style-h-1">TIME</h1>
